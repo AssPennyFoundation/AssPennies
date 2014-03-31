beffbc84e9fdeb364b19511e37058159db1fdf85
hugheml@gmail.com <hugheml@gamil.com>
2014-03-31 13:25:54 -0400

    Update first char of address to A

--- src/base58.h
+++ src/base58.h
@@ -277,7 +277,7 @@ class CBitcoinAddress : public CBase58Data
 public:
     enum
     {
-        PUBKEY_ADDRESS = 9, //Set the address first bit here
+        PUBKEY_ADDRESS = 23, //Set the address first bit here
         SCRIPT_ADDRESS = 5,
         PUBKEY_ADDRESS_TEST = 111,
         SCRIPT_ADDRESS_TEST = 196,
