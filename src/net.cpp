6eb991e62750accb18824f492916e4518632b5e8
Matt Hughes <matthughes.tech@gmail.com>
2017-09-15 16:26:37 -0400

    Update seed node to new mothership IP

--- src/net.cpp
+++ src/net.cpp
@@ -997,7 +997,7 @@ void ThreadSocketHandler2(void* parg)
 // The first name is used as information source for addrman.
 // The second name should resolve to a list of seed addresses.
 static const char *strDNSSeed[][2] = {
-    {"AssPenny Mothership", "matthughes.io"},
+    {"AssPennies Mothership", "52.173.17.12"},
 };
 
 void ThreadDNSAddressSeed(void* parg)
